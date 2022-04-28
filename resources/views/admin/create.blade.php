fb871ce078db8c497adbd222b42ed6ed4f83c36e
camilo-mancipe <44626685+keymiro@users.noreply.github.com>
2022-04-28 13:25:11 -0500

    update

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -1,38 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header bg-dark text-white">{{ __('Register') }}</div>
+                <div class="card-header bg-dark text-white">Nuevo </div>
 
                 <div class="card-body">
                     <form class="row g-3" method="POST" action="{{route('admin.store')}}">
                         @csrf
                         <div class="col-md-6">
                           <label for="inputEmail4" class="form-label">Email</label>
-                          <input type="email" class="form-control" id="inputEmail4" name="email">
+                          <input type="email" class="form-control" required id="inputEmail4" name="email">
                         </div>
                         <div class="col-md-6">
                           <label for="inputPassword4" class="form-label">Contraseña</label>
-                          <input type="password" class="form-control" id="inputPassword4" name="password">
+                          <input type="password" class="form-control" required id="inputPassword4" name="password">
                         </div>
                         <div class="col-md-6">
                             <label for="inputEmail4" class="form-label">Nombres</label>
-                            <input type="text" class="form-control" id="inputEmail4" name="names">
+                            <input type="text" class="form-control" required id="inputEmail4" name="names">
                           </div>
                           <div class="col-md-6">
                             <label for="inputPassword4" class="form-label">Apellidos</label>
-                            <input type="text" class="form-control" id="inputPassword4" name="lastnames">
+                            <input type="text" class="form-control" required id="inputPassword4" name="lastnames">
                           </div>
                         <div class="col-md-6">
                           <label for="inputCity" class="form-label">Documento</label>
-                          <input type="text" class="form-control" id="inputCity" name="document">
+                          <input type="text" class="form-control" required id="inputCity" name="document">
                         </div>
                         <div class="col-md-4">
                           <label for="inputState" class="form-label">Rol</label>
-                          <select id="inputState" class="form-select" name="rol">
+                          <select id="inputState" class="form-select" name="rol" required>
                             <option selected>Seleccione un rol</option>
                             <option value="super-admin">super-admin</option>
                             <option value="admin">admin</option>
@@ -40,10 +40,10 @@
                         </div>
                         <div class="col-md-4">
                             <label for="inputState" class="form-label">Estado</label>
-                            <select id="inputState" class="form-select" name="state">
+                            <select id="inputState" class="form-select" name="state" required>
                               <option selected>Seleccione un estado</option>
-                              <option value="1">Activo</option>
-                              <option value="0">Inactivo</option>
+                              <option value="activo">activo</option>
+                              <option value="inactivo">inactivo</option>
                             </select>
                           </div>
                         <div class="col-12">
